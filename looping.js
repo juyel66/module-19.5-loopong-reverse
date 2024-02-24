d538ca228c1c2328f785be7f32582f133e344884
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-02-24 13:00:25 +0600

    'module-19.5'

---
+++ looping.js
@@ -0,0 +1,26 @@
+// Looping technique
+
+// 1.for loop
+// 2.do while
+// 3.for in     //object loop korar jnno use kora hoy
+// 4.for of    //array loop korar jnno use kora hoy 
+
+
+
+const friends= ['Elon', 'mark', 'bill', 'waren'];
+
+for( const friend of friends){
+    // console.log(friend);
+}
+
+for( let i=0; i<friends.length; i++){
+    console.log(i);
+    console.log(friends[i]);
+}
+
+const numbers=[10, 20, 34, 23,44,34,23,23,43,23,32,34,4,123,23,];
+for( const number of number )
+for(let n=0; n<numbers.length; n++){
+    console.log(n);
+    console.assert;log(numbers[n]);
+}
---
+++ reverse.js
@@ -0,0 +1,14 @@
+const number =[1,2,3,4,5,6,7,8,9];
+// console.log(number);
+
+
+// number.reverse();
+// console.log(number);
+
+
+// const array_reverse =number.reverse();
+// console.log(number)
+
+for ( const num of number){
+    console.log(number[num]);
+}
